11926d602e9de657b9662d1841363c95d6bb9400
RajiReddem <raji.reddem24@gmail.com>
2023-10-05 02:05:25 -0400

    TDD: Added integer to roman Param test.

--- src/test/java/com/ford/us/roman/util/RomanTest.java
+++ src/test/java/com/ford/us/roman/util/RomanTest.java
@@ -25,51 +25,56 @@ class RomanTest {
     }
 
 
-
-
-    @Test
-    void convertIntegerToRoman_success()
-    {
-        Integer input=102;
-        assertEquals("CII",Roman.toRoman(input));
+    @ParameterizedTest()
+    @MethodSource("getIntegerToRoman")
+    void convertIntegerToRomanParam_success_map(Map<String, Integer> inputMap) {
+        inputMap.forEach((k,v) ->assertEquals(k, Roman.toRoman(v)));
     }
+
     @Test
-    void convertRomanToInteger_success()
-    {
-        String input="XI";
-        assertEquals(11,Roman.toInteger(input));
+    void convertIntegerToRoman_success() {
+        Integer input = 102;
+        assertEquals("CII", Roman.toRoman(input));
+    }
+
+    @Test
+    void convertRomanToInteger_success() {
+        String input = "XI";
+        assertEquals(11, Roman.toInteger(input));
     }
 
     @ParameterizedTest
     @MethodSource("getRomanLetterAndValueMap")
-    void convertRomanToInteger_success_map(Map<String,Integer> inputMap)
-    {
-        inputMap.forEach((k,v)-> assertEquals(Roman.toInteger(k),v));
+    void convertRomanToInteger_success_map(Map<String, Integer> inputMap) {
+        inputMap.forEach((k, v) -> assertEquals(Roman.toInteger(k), v));
     }
 
 
-
     @ParameterizedTest()
-    @ValueSource(strings = { "","abc"})
+    @ValueSource(strings = {"", "abc"})
     @NullSource
-    void convertRomanToInteger_failure(String paramInput)
-    {
+    void convertRomanToInteger_failure(String paramInput) {
 
         InvalidInputException invalidInputException = assertThrowsExactly(InvalidInputException.class, () -> Roman.toInteger(paramInput));
 
         String message = invalidInputException.getMessage();
 
-        assertEquals("{} Invalid Input ".formatted(paramInput),message);
+        assertEquals("{} Invalid Input ".formatted(paramInput), message);
     }
 
 
-    static Stream<Map<String, Integer>> getRomanLetterAndValueMap ()
-    {
-        return Stream.of(Map.of("X",10,
-                "MMM",3000,
+    static Stream<Map<String, Integer>> getRomanLetterAndValueMap() {
+        return Stream.of(Map.of("X", 10,
+                "MMM", 3000,
                 "L", 50,
                 "LI", 51,
                 "IL", 49));
+    }
+
+    static Stream<Map<String, Integer>> getIntegerToRoman() {
+        return Stream.of(Map.of("X", 10,
+                "MMM", 3000,
+                "XLIX", 49));
     }
 
 
