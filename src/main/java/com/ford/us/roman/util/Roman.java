36001a7903714832a0cd78504ff5159153082f43
RajiReddem <raji.reddem24@gmail.com>
2023-10-05 02:18:35 -0400

    TDD: code cleanup

--- src/main/java/com/ford/us/roman/util/Roman.java
+++ src/main/java/com/ford/us/roman/util/Roman.java
@@ -2,39 +2,13 @@ package com.ford.us.roman.util;
 
 import com.ford.us.roman.exception.InvalidInputException;
 
-import java.util.*;
+import java.util.OptionalInt;
 
-import static com.ford.us.roman.util.RomanConstant.INVALID_INPUT;
+import static com.ford.us.roman.util.RomanConstant.*;
 import static java.util.Objects.isNull;
 
 public final class Roman {
 
-    private static final Map<Integer, String> ROMAN_INT_VALUES = getRomanMap();
-    private static Map<Integer, String> getRomanMap() {
-        Map<Integer, String> map = new LinkedHashMap<>();
-        map.put(1000, "M");
-        map.put(900, "CM");
-        map.put(500, "D");
-        map.put(400, "CD");
-        map.put(100, "C");
-        map.put(90, "XC");
-        map.put(50, "L");
-        map.put(40, "XL");
-        map.put(10, "X");
-        map.put(9, "IX");
-        map.put(5, "V");
-        map.put(4, "IV");
-        map.put(1, "I");
-        return map;
-    }
-
-    private static final Map<Character, Integer>
-            ROMAN_LETTER_MAP = Map.of(
-            'I', 1, 'V', 5, 'X', 10,
-            'L', 50, 'C', 100, 'D', 500,
-            'M', 1000);
-
-    private static final Set<Character> ALLOWED_ROMAN_CHARS = ROMAN_LETTER_MAP.keySet();
 
     private Roman() {
 
--- src/main/java/com/ford/us/roman/util/RomanConstant.java
+++ src/main/java/com/ford/us/roman/util/RomanConstant.java
@@ -1,5 +1,9 @@
 package com.ford.us.roman.util;
 
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Set;
+
 public final class RomanConstant {
 
     private RomanConstant()
@@ -7,5 +11,33 @@ public final class RomanConstant {
 
     }
 
-    public static String INVALID_INPUT="{} Invalid Input ";
+    public static final String INVALID_INPUT="{} Invalid Input ";
+
+
+    public static final Map<Integer, String> ROMAN_INT_VALUES = getRomanMap();
+    public static Map<Integer, String> getRomanMap() {
+        Map<Integer, String> map = new LinkedHashMap<>();
+        map.put(1000, "M");
+        map.put(900, "CM");
+        map.put(500, "D");
+        map.put(400, "CD");
+        map.put(100, "C");
+        map.put(90, "XC");
+        map.put(50, "L");
+        map.put(40, "XL");
+        map.put(10, "X");
+        map.put(9, "IX");
+        map.put(5, "V");
+        map.put(4, "IV");
+        map.put(1, "I");
+        return map;
+    }
+
+    public static final Map<Character, Integer>
+            ROMAN_LETTER_MAP = Map.of(
+            'I', 1, 'V', 5, 'X', 10,
+            'L', 50, 'C', 100, 'D', 500,
+            'M', 1000);
+
+    public static final Set<Character> ALLOWED_ROMAN_CHARS = ROMAN_LETTER_MAP.keySet();
 }
